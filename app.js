ae947383546647993342fcb860031d1d79cde4a0
faizan2777 <faizan@troontechnologies.com>
2023-02-18 16:19:38 +0500

    updates middlewares

--- app.js
+++ app.js
@@ -1,106 +1,112 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const { Strategy } = require('passport-local');
-var cors = require('cors');
-const multer = require('multer');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const { Strategy } = require("passport-local");
+var cors = require("cors");
+const multer = require("multer");
 
-const inputMiddleware = require('./middlewares/inputMiddleware');
+const inputMiddleware = require("./middlewares/inputMiddleware");
 const {
   userRoute,
   adminRoute,
   clientRoute,
   clientDocumentsRoute,
-  adminDocumentRoutes
-} = require('./routes');
+  adminDocumentRoutes,
+} = require("./routes");
 
-
-const authMiddleware = require('./middlewares/authMiddleware');
+const authMiddleware = require("./middlewares/authMiddleware");
 
 var app = express();
-const upload = multer({ dest: 'uploads/' });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); //Appending .jpg
+  },
+});
 
- 
+var upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
+
 //DB Connection
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://127.0.0.1:27017/my-db',(err) => {
-  if (err){
-    return console.log('error connecting with DB' , err);
-    
+mongoose.connect("mongodb://127.0.0.1:27017/my-db", (err) => {
+  if (err) {
+    return console.log("error connecting with DB", err);
   }
-  console.log('DateBase connected successfully');
-
+  console.log("DateBase connected successfully");
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-passport.use(new Strategy(
-  (username, password, done) => {
+passport.use(
+  new Strategy((username, password, done) => {
     authMiddleware.executeLogin(username, password, done);
-  }
-));
+  })
+);
 
 // actual routes
 // app.use(inputMiddleware.handleOptions);
-app.post('/signup', authMiddleware.userSignup);
-app.post('/login',
+app.post("/signup", authMiddleware.userSignup);
+app.post(
+  "/login",
   passport.initialize(),
-  passport.authenticate('local', {
+  passport.authenticate("local", {
     session: false,
-    scope: []
+    scope: [],
   }),
   authMiddleware.generateToken,
   authMiddleware.respond
 );
-app.post('/upload',upload.single('file'),function (req, res, next){
+app.post("/upload", upload.single("file"), function (req, res, next) {
   return res.status(200).json(req.file);
 });
 // test routes
 // app.use('/', indexRouter);
-app.use('/users', userRoute);  //Mount userRoute in express
-app.use('/admin' , adminRoute);  //Mount adminRoute in express
-app.use('/client' , clientRoute);   //Mount clientRoute in express
+app.use("/users", userRoute); //Mount userRoute in express
+app.use("/admin", adminRoute); //Mount adminRoute in express
+app.use("/client", clientRoute); //Mount clientRoute in express
 app.use(authMiddleware.verifyToken);
 app.use(
-  '/client/documents',
+  "/client/documents",
   authMiddleware.checkClientPermissions,
   clientDocumentsRoute
 );
 app.use(
-  '/admin/documents',
+  "/admin/documents",
   authMiddleware.checkAdminPermissions,
   adminDocumentRoutes
 );
 
-
 // catch 404 and forward to error handler
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,33 +1,37 @@
-const { sign, verify } = require('jsonwebtoken');
-const { signup } = require('../beans/common');
-const { usersController, adminController, clientController } = require('../controllers');
+const { sign, verify } = require("jsonwebtoken");
+const { signup } = require("../beans/common");
+const {
+  usersController,
+  adminController,
+  clientController,
+} = require("../controllers");
 
 const executeLogin = async (username, password, cb) => {
-    try {
-        const filter = { userName: username, password:password };
-        const user = await usersController.getUser(filter);
-        if (!user) {
-            return cb(null, false);
-        }
-
-        return cb(null, user);
-    } catch (error) {
-        return cb(error, false);
+  try {
+    const filter = { userName: username, password: password };
+    const user = await usersController.getUser(filter);
+    if (!user) {
+      return cb(null, false);
     }
+
+    return cb(null, user);
+  } catch (error) {
+    return cb(error, false);
+  }
 };
 const generateToken = async (req, res, next) => {
-    const user = req.user;
+  const user = req.user;
 
-    const json = {
-        _id: user._id
-    };
-    const token = sign({ user: json }, 'someSecretvalue',{expiresIn:"1h"});
-    req.token = token;
-    next();
+  const json = {
+    _id: user._id,
+  };
+  const token = sign({ user: json }, "someSecretvalue", { expiresIn: "1h" });
+  req.token = token;
+  next();
 };
 const respond = async (req, res, next) => {
-    const user = req.user;
-/*
+  const user = req.user;
+  /*
     const userType = user.userType.kind;
     const item = user.userType.item;
     let data = null;
@@ -40,97 +44,100 @@ const respond = async (req, res, next) => {
    const result = { token: req.token, user: data };
     res.status(200).send(result);
     */
-    const result = { token: req.token, user: user.userType.item };
-    res.status(200).send(result);
+  const result = { token: req.token, user: user.userType };
+  res.status(200).send(result);
 };
-const userSignup = async (req, res, next) => { 
-    const body = req.body;
-    try {
-        const result = await signup(body);
-        return res.status(200).send(result);
-    } catch (error) {
-        return res.status(400).send(error);
-    }
+const userSignup = async (req, res, next) => {
+  const body = req.body;
+  try {
+    const result = await signup(body);
+    return res.status(200).send(result);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 };
 
 function verifyToken(req, res, next) {
-    let token = '';
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Bearer'
-    ) {
-      // eslint-disable-next-line prefer-destructuring
-      token = req.headers.authorization.split(' ')[1];
-    } else if (req.query.token) {
-      token = req.query.token;
-    } else {
-      return res
-        .status(401)
-        .json({ success: false, error: 'Please provide token.' });
-    }
-    verify(token, "someSecretvalue", async (error, decoded) => {
-      
+  let token = "";
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    // eslint-disable-next-line prefer-destructuring
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.query.token) {
+    token = req.query.token;
+  } else {
+    return res
+      .status(401)
+      .json({ success: false, error: "Please provide token." });
+  }
+  verify(token, "someSecretvalue", async (error, decoded) => {
     console.log(error, decoded);
-      if(error) {
-        const { name } = error;
-        switch (name) {
-          case 'JsonWebTokenError':
-            return res
-              .status(401)
-              .json({ success: false, error: 'Request Access Denied!' });
-            break;
-          case 'TokenExpiredError':
-            return res
-              .status(401)
-              .json({ success: false, error: 'Your session has been expired!' });
-            break;
-          default:
-            return res
-              .status(401)
-              .json({ success: false, error: 'unhandled authentication case!' });
-        }
-      } else {
-        const userID = decoded?.user?._id;
-        try {
-          let user = await usersController.getUser({_id : userID});
-          if (user === null) {
-            return res
-              .status(401)
-              .json({ success: false, error: 'Invalid token' });
-          }
-          req.user = user;
-          next();
-        } catch (err) {
-          res.status(401).send(err);
-        }
+    if (error) {
+      const { name } = error;
+      switch (name) {
+        case "JsonWebTokenError":
+          return res
+            .status(401)
+            .json({ success: false, error: "Request Access Denied!" });
+          break;
+        case "TokenExpiredError":
+          return res
+            .status(401)
+            .json({ success: false, error: "Your session has been expired!" });
+          break;
+        default:
+          return res
+            .status(401)
+            .json({ success: false, error: "unhandled authentication case!" });
       }
-    });
+    } else {
+      const userID = decoded?.user?._id;
+      try {
+        let user = await usersController.getUser({ _id: userID });
+        if (user === null) {
+          return res
+            .status(401)
+            .json({ success: false, error: "Invalid token" });
+        }
+        req.user = user;
+        next();
+      } catch (err) {
+        res.status(401).send(err);
+      }
+    }
+  });
 }
 
 function checkClientPermissions(req, res, next) {
   const userType = req.user.userType.kind;
-  if (userType === 'client') {
+  if (userType === "client") {
     next();
   } else {
-    return res.status(401).send({ message: "You do not have permissions to access this" });
+    return res
+      .status(401)
+      .send({ message: "You do not have permissions to access this" });
   }
-};
+}
 
 function checkAdminPermissions(req, res, next) {
   const userType = req.user.userType.kind;
-  if (userType === 'admin') {
+  if (userType === "admin") {
     next();
   } else {
-    return res.status(401).send({ message: "You do not have permissions to access this" });
+    return res
+      .status(401)
+      .send({ message: "You do not have permissions to access this" });
   }
-};
+}
 
 module.exports = {
-    userSignup,
-    executeLogin,
-    generateToken,
-    respond,
-    verifyToken,
-    checkAdminPermissions,
-    checkClientPermissions
-}
+  userSignup,
+  executeLogin,
+  generateToken,
+  respond,
+  verifyToken,
+  checkAdminPermissions,
+  checkClientPermissions,
+};
